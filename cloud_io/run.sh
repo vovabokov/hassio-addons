0e424dffeaff0e41dc38007aba81ba5b311b6980
vovabokov <107910463+vovabokov@users.noreply.github.com>
2026-07-08 20:54:18 +0300

    Update run.sh

--- cloud_io/run.sh
+++ cloud_io/run.sh
@@ -1,8 +1,6 @@
-#!/usr/bin/with-contenv bashio
-
+#!/bin/bash
 echo "cloud_io (c) Alex Bokov 2022-2024"
 
-# Generate key
 KEY_PATH=/data/ssh_keys
 if [ ! -d "$KEY_PATH" ]; then
     echo "[INFO] Setup private key"
@@ -16,15 +14,15 @@ echo "[INFO] public key is:"
 cat "${KEY_PATH}/autossh_ed25519.pub"
 
 echo "[INFO] json config is:"
-cat /data/options.json 
+cat /data/options.json
 
-client_id=$(bashio::config 'client_id')
-client_ssh=$(bashio::config 'client_ssh')
-router_webui=$(bashio::config 'router_webui')
+client_id=$(jq -r ".client_id" /data/options.json)
+client_ssh=$(jq -r ".client_ssh" /data/options.json)
+router_webui=$(jq -r ".router_webui" /data/options.json)
 
-# Get network info using bashio
-hassio_ip=$(bashio::network.ipv4_address)
-router_ip=$(bashio::network.gateway)
+curl -s -X GET -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" -H "Content-Type: application/json" http://supervisor/network/info > /tmp/networkinfo
+hassio_ip=$(jq -r ".data.interfaces[] | .ipv4.address[]" /tmp/networkinfo | awk -F/ '{print $1}')
+router_ip=$(jq -r ".data.interfaces[] | .ipv4.gateway | select( . != null )" /tmp/networkinfo)
 
 cloud_hostname='cloud.uzvhost.ru'
 cloud_username='cloudio'
@@ -38,7 +36,7 @@ do
     echo "[INFO] testing cloud ssh connection"
     ssh -o StrictHostKeyChecking=no -p $cloud_ssh_port $cloud_hostname 2>/dev/null || true
 
-    if [ "$client_ssh" = true ]; then
+    if [ "$client_ssh" = "true" ]; then
         ssh_control_port=$((client_id+2))
         ssh_monitor_port=$((control_port+3))
         command_args="-M ${ssh_monitor_port} -R 0.0.0.0:${ssh_control_port}:${hassio_ip}:22 -N -q -o ServerAliveInterval=5 -o ServerAliveCountMax=1 ${cloud_username}@${cloud_hostname} -p ${cloud_ssh_port} -i ${KEY_PATH}/autossh_ed25519"
@@ -46,7 +44,7 @@ do
         /usr/bin/autossh ${command_args} &
     fi
 
-    if [ "$router_webui" = true ]; then
+    if [ "$router_webui" = "true" ]; then
         router_webui_control_port=$((client_id+4))
         router_webui_monitor_port=$((control_port+5))
         command_args="-M ${router_webui_monitor_port} -R 0.0.0.0:${router_webui_control_port}:${router_ip}:80 -N -q -o ServerAliveInterval=5 -o ServerAliveCountMax=1 ${cloud_username}@${cloud_hostname} -p ${cloud_ssh_port} -i ${KEY_PATH}/autossh_ed25519"
@@ -57,7 +55,7 @@ do
     command_args="-M ${monitor_port} -R 0.0.0.0:${control_port}:${hassio_ip}:8123 -N -q -o ServerAliveInterval=5 -o ServerAliveCountMax=1 ${cloud_username}@${cloud_hostname} -p ${cloud_ssh_port} -i ${KEY_PATH}/autossh_ed25519"
     echo "[INFO] command args: ${command_args}"
     /usr/bin/autossh ${command_args} || true
-    
+
     echo "repeating in 60 sec..."
     sleep 60
 done
